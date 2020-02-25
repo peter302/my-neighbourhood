745bafea80f53cbab2db99614f6b5852e083fc51
peter kuria <you@example.com>
2020-02-25 07:10:59 -0500

    added index view

--- my/views.py
+++ my/views.py
@@ -3,3 +3,8 @@ from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .forms import *
+
+
+def index(request):
+    hoods = Neighborhood.objects.all()
+    return render(request, 'main/index.html', {'hoods':hoods})

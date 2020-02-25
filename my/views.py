50103ebb0602c9f253b207d46d745cc4f0f9758f
peter kuria <you@example.com>
2020-02-25 07:10:17 -0500

    made several imports to views.py

--- my/views.py
+++ my/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import *

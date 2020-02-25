d0a5c191c0df13f21251d815cc2ebb273603e29b
peter kuria <you@example.com>
2020-02-25 06:45:05 -0500

    created neighbourhood model

--- my/models.py
+++ my/models.py
@@ -3,3 +3,13 @@ from django.contrib.auth.models import User
 from pyuploadcare.dj.models import ImageField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=50)
+    occupants = models.PositiveIntegerField()
+    health_contact = models.PositiveIntegerField()
+    police_contact = models.PositiveIntegerField()
+    hood_pic = models.ImageField(upload_to='images/', blank=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)

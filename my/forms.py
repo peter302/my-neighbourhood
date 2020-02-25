a339a42124e691df702240458fcce93aa2daa40b
peter kuria <you@example.com>
2020-02-25 07:27:11 -0500

    added a postform

--- my/forms.py
+++ my/forms.py
@@ -2,3 +2,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude=['user']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields =['title', 'content', 'image']

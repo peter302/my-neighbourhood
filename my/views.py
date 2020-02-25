cd23a7ca58e51b98f030131c67b277a4c127bf44
peter kuria <you@example.com>
2020-02-25 07:13:34 -0500

    added update profile view

--- my/views.py
+++ my/views.py
@@ -21,4 +21,17 @@ def profile(request):
       profile = Profile.objects.filter(user=current_user).first()
       posts = request.user.post_set.all()
 
-      return render(request, 'main/profile.html', {"posts": posts, "profile": profile, 'current_user':current_user})        
+      return render(request, 'main/profile.html', {"posts": posts, "profile": profile, 'current_user':current_user})
+
+
+def update_profile(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == "POST":
+        form =  ProfileForm(request.POST,request.FILES,instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+        return render(request,'main/update_profile.html', {'form':form})

42c61443b90d7323e63e6f47e30166b4c501d288
peter kuria <you@example.com>
2020-02-25 07:35:47 -0500

    added a profile testclass and associated methods

--- my/tests.py
+++ my/tests.py
@@ -2,3 +2,30 @@ from django.test import TestCase
 
 # Create your tests here.
 from .models import *
+
+
+
+class ProfileTestClass(TestCase):
+    #Set up method
+
+    def setUp(self):
+
+        self.new_profile = Profile(user_id=2,hood_id=3,bio="just testing", email='titusouko@gmail.com',name="Titus",profile_pic="image.jpeg")
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+
+    def test_save_method(self):
+        self.new_profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def test_delete_method(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
